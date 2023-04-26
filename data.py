b1dfe4355ac55f439b8f83de14f5f8476539fff2
PrachetShah <prachetshah25@gmail.com>
2023-04-26 23:18:42 +0530

    new data created

--- data.py
+++ data.py
@@ -1,21 +1,31 @@
 from faker import Faker
 from faker.providers import DynamicProvider
+from contentBased import *
 import pandas as pd
 import random
 
 data = pd.read_csv('places.csv')
 cities = list(set(data['City']))
-print(len(cities))
+places = list(set(data['Place']))
+print(len(cities), len(places))
 
 generated_cities = []
 generated_ages = []
 generated_categories = []
 generated_gender = []
+generated_Splace = []
+generated_Dplace = []
 
 # for city generation
 city_names_provider = DynamicProvider(
      provider_name="city_generation",
      elements=cities,
+)
+
+# for place generation
+place_names_provider = DynamicProvider(
+     provider_name="place_generation",
+     elements=places,
 )
 
 # for category generation
@@ -24,26 +34,75 @@ categories_names_provider = DynamicProvider(
      elements=['Natural', 'Historical', 'Religious', 'Architectural'],
 )
 
-fake = Faker(['en_IN'])
-# then add new provider to faker instance
-fake.add_provider(city_names_provider)
-fake.add_provider(categories_names_provider)
+def data1():
+     fake = Faker(['en_IN'])
+     # then add new provider to faker instance
+     fake.add_provider(city_names_provider)
+     fake.add_provider(categories_names_provider)
 
-for _ in range(65):
-    city = fake.city_generation()
-    age = random.randint(10, 65)
-    category = fake.category_generation()
-    gender = random.choice(["Male", "Female"])
+     for _ in range(150):
+          city = fake.city_generation()
+          age = random.randint(10, 65)
+          category = fake.category_generation()
+          gender = random.choice(["Male", "Female"])
 
-    generated_cities.append(city)
-    generated_ages.append(age)
-    generated_categories.append(category)
-    generated_gender.append(gender)
-    # print(city, age, category)
+          generated_cities.append(city)
+          generated_ages.append(age)
+          generated_categories.append(category)
+          generated_gender.append(gender)
+          # print(city, age, category)
 
-# Calling DataFrame constructor on list
-data = {'City':generated_cities, 'Category':generated_categories, 'Age':generated_ages, 'Gender':generated_gender}
-df = pd.DataFrame(data)
-print(df.head())
+     # Calling DataFrame constructor on list
+     data = {'City':generated_cities, 'Category':generated_categories, 'Age':generated_ages, 'Gender':generated_gender}
+     df = pd.DataFrame(data)
+     print(df.head())
 
-df.to_csv('profile_data.csv')
+     df.to_csv('profile_data.csv')
+
+def data2():
+     fake = Faker(['en_IN'])
+     # then add new provider to faker instance
+     fake.add_provider(city_names_provider)
+     fake.add_provider(place_names_provider)
+     fake.add_provider(categories_names_provider)
+
+     df_places = pd.read_csv('places.csv')
+     Splace = fake.place_generation()
+     print(Splace)
+     # places = Splace
+     output = {}
+     # Place id is [2251 'Mumbai' 'Marine Drive' 7500 1635 18] 
+     id = df_places[df_places['Place']==Splace].values[0]
+     # print(df_places[df_places['Place']==place]['PlaceID'])
+     output[Splace] = recommend(id[3], num=1)
+     print(output[Splace]['Suggested'][0][0])
+
+     for _ in range(150):
+          city = fake.city_generation()
+          age = random.randint(10, 65)
+          category = fake.category_generation()
+          gender = random.choice(["Male", "Female"])
+
+          Splace = fake.place_generation()
+          id = df_places[df_places['Place']==Splace].values[0]
+          place2 = recommend(id[3], num=1)
+          Dplace = place2['Suggested'][0][0]
+          # print(Splace, Dplace)
+
+          generated_cities.append(city)
+          generated_Splace.append(Splace)
+          generated_Dplace.append(Dplace)
+          generated_ages.append(age)
+          generated_categories.append(category)
+          generated_gender.append(gender)
+          # print(city, age, category)
+
+     # Calling DataFrame constructor on list
+     data = {'City':generated_cities, 'SPlace':generated_Splace, 'DPlace':generated_Dplace, 'Category':generated_categories, 'Age':generated_ages, 'Gender':generated_gender}
+     df = pd.DataFrame(data)
+     print(df.head())
+
+     df.to_csv('feedback_data.csv')
+
+data1()
+data2()
